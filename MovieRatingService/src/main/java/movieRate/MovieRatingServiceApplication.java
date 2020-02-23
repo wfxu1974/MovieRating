33d63e31653bb5f43e097ab0da46c9f423d59f07
muren <muren@LAPTOP-A96KPT54>
2020-02-23 14:45:13 -0500

    second commit

--- MovieRatingService/src/main/java/movieRate/MovieRatingServiceApplication.java
+++ MovieRatingService/src/main/java/movieRate/MovieRatingServiceApplication.java
@@ -2,9 +2,10 @@ package movieRate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MovieRatingServiceApplication {
+public class MovieRatingServiceApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieRatingServiceApplication.class, args);
